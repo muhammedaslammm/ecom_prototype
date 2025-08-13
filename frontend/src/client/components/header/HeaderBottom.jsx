555697e8d7a2b688b2cadab7dc8a48d757d474ce
Muhammed Aslam <muhammedbinramli@gmail.com>
2025-08-13 13:22:04 +0530

    url changed

--- frontend/src/client/components/header/HeaderBottom.jsx
+++ frontend/src/client/components/header/HeaderBottom.jsx
@@ -4,14 +4,10 @@ import { CategoryContext } from "../../../contexts";
 const HeaderBottom = () => {
   const { navbarCategories } = useContext(CategoryContext);
   return (
-    <nav className="bg-white ">
+    <nav className="bg-white">
       <ul className="flex w-[90%] mx-auto text-black">
         {navbarCategories.map((category) => (
-          <>
-            <div className="group">
-              <div className="text--body">{category.title}</div>
-            </div>
-          </>
+          <div>{category.title}</div>
         ))}
       </ul>
     </nav>
