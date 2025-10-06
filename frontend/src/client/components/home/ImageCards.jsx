d989be2cc38706ae93f44ebedb5db8f98753a0f9
Muhammed Aslam <muhammedbinramli@gmail.com>
2025-10-06 07:14:02 +0530

    (data fetch):fetched data from backend

--- frontend/src/client/components/home/ImageCards.jsx
+++ frontend/src/client/components/home/ImageCards.jsx
@@ -3,18 +3,17 @@ import { useEffect, useState } from "react";
 const ImageCards = ({ categoryID }) => {
   let [products, setProducts] = useState([]);
   let BACKEND_URL = import.meta.env.VITE_BACKEND_URL_1;
-  // so far, products are only called based on category
   useEffect(() => {
     let getProducts = async () => {
       try {
         let response = await fetch(
-          `${BACKEND_URL}/api/products?filter=home&cateogry=${categoryID}&product_limit=5`,
+          `${BACKEND_URL}/api/products?filter=home&category=${categoryID}&product_limit=5`,
           { method: "GET" }
         );
         let data = await response.json();
         if (!response.ok) throw new Error(data.message);
         else {
-          console.log("products:", [data.products]);
+          console.log("products:", data.products);
         }
       } catch (error) {
         console.error(error.message);
